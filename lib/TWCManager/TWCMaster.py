b05efc405907af8bab2fdf4f483e0afa07a78285
Frajul <frajul@comumail.de>
2020-10-20 19:25:45 +0200

    Removed more print

--- lib/TWCManager/TWCMaster.py
+++ lib/TWCManager/TWCMaster.py
@@ -1092,7 +1092,6 @@ class TWCMaster:
             self.saveSettings()
 
     def startCarsCharging(self):
-        print("start cars charging")
         # This function is the opposite functionality to the stopCarsCharging function
         # below
         stopMode = int(self.settings.get("chargeStopMode", 1))
@@ -1106,7 +1105,6 @@ class TWCMaster:
             self.queue_background_task({"cmd": "charge", "charge": True})
 
     def stopCarsCharging(self):
-        print("stop cars charging")
         # This is called by components (mainly TWCSlave) who want to signal to us to
         # call our configured routine for stopping vehicles from charging.
         # The default setting is to use the Tesla API. Some people may not want to do
